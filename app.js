28f178eb46f5503ab9d2924e348cc26db0a4693a
Christian Panter <pantercp@outlook.com>
2023-01-08 17:32:51 +0000

    Added carousel feature for testimonials

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const dropMenu = document.querySelector('.container-drop');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if (!menuOpen) {
     menuBtn.classList.add('open');
     dropMenu.classList.add('open');
     menuOpen = true;
@@ -12,3 +12,22 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
